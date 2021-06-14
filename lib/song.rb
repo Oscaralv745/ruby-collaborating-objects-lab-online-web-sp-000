74ec8973f293edb0b17f3e2b6a2d7601165b1785
Oscar Alvarez <gavinalv6@gmail.com>
2021-06-14 19:33:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,10 +4,16 @@ class Song
 
   def initialize(name)
     @name = name
-    @@all << self 
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def self.new_by_filename(filename)
+   song = self.new
+   song.name = filename.split(" - ")[1]
+   song
   end
 end
