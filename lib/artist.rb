8e3cb17c0151631bde9d7fc6a7901a494b52e064
Oscar Alvarez <gavinalv6@gmail.com>
2021-06-14 19:32:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,9 +25,10 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    song = self.new
-    song.name = name.split
-    save 
-    song
+
+  end
+
+  def print_songs
+    
   end
 end
